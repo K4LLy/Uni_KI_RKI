541b77ceca4ffb5680b14b2bffe7abb755297d77
K4LLy <k4llylp@gmail.com>
2020-06-17 15:52:26 +0200

    modifed .gitignore DataFrame restored

--- Heatmap.py
+++ Heatmap.py
@@ -3,6 +3,31 @@ import csv
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(timeout = None)
 
+def generateOld(covid_data, file_name = "Heatmap"):
+    data = covid_data.groupby(['Bundesland']).sum()
+
+    #Karte mit Fokus auf Deutschland 
+    folium_map = folium.Map(location=[51.144, 9.902],
+                            zoom_start=6.5,
+                            tiles="CartoDB positron")
+
+    #pro Bundesland Kreis zeichnen
+    for bundesland, row in data.iterrows():
+        anzahl_faelle = row['AnzahlFall']
+        anzeige_string = str(anzahl_faelle) + " Fälle insgesamt in " + bundesland
+        location = geolocator.geocode(bundesland) #mit dem Geolocator können die Koordinaten von dem Bundesland hergeholt werden
+        folium.Circle(
+            location = [location.latitude, location.longitude], #Kreis liegt auf dem Zentrum des Bundeslandes
+            popup = anzeige_string,
+            radius = float(anzahl_faelle) * 2, #Radius ist von den Fallzahlen abhängig
+            color = 'red',
+            fill = True,
+            fill_color = 'red'
+            ).add_to(folium_map) #hinzufügen der Kreise auf die Karte
+
+    #Speichern in HTML
+    folium_map.save("Result\\" + file_name + ".html")
+
 def generate(file_name = "Heatmap"):    
 
     with open('Data\\RKI_COVID19.csv') as csvdatei:
--- Main.py
+++ Main.py
@@ -2,16 +2,16 @@ import DataReader as reader
 import Heatmap as hm
 import Charts as ch
 import Heatmap_chart_test as hmt
-import HeatmapRichtig as hmr
+#import HeatmapRichtig as hmr
 
 data = reader.getCovidData()
-test = hmr.generate()
-print(test)
+#test = hmr.generate()
+#print(test)
 
-ch.generate_pie_chart(data)
-ch.generate_bar_chart(data)
-ch.generate_graph(data)
+#ch.generate_pie_chart(data)
+#ch.generate_bar_chart(data)
+#ch.generate_graph(data)
 
-hm.generate("Map")
+hm.generateOld(data, "Map")
 
-hmt.generate()
+#hmt.generate()
