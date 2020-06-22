44bed8629c064cce24250d3d62d37b6361a5eb44
K4LLy <k4llylp@gmail.com>
2020-06-22 12:57:18 +0200

    added metric

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 import DataReader as reader
 import Heatmap as hm
 import Charts as ch
+import LinReg as lr
 
 data = reader.get_covid_data()
 
-ch.generate_pie_chart(data)
-ch.generate_bar_chart(data)
-ch.generate_graph(data)
+#ch.generate_pie_chart(data)
+#ch.generate_bar_chart(data)
+#ch.generate_graph(data)
 
-hm.generate_chart(data)
-hm.generate_circle(data)
-hm.generate_heatmap(data)
+#hm.generate_chart(data)
+#hm.generate_circle(data)
+#hm.generate_heatmap(data)
+
+#lr.predict(data, 15, 70, 80, 'AnzahlGenesen')
+lr.predict(data, 15, 40, 80, 'AnzahlFall', True)
